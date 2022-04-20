896a014cc58548b91b05b31425a0fc5b22d0c736
Parsa Hosseini <parsa@pop-os.localdomain>
2022-04-20 14:50:54 +0430

    Add codereviewvideos route and test

---
+++ src/routes/codereviewvideos.ts
@@ -0,0 +1,19 @@
+import { Context } from "koa";
+import Router from "koa-router";
+
+const router = new Router();
+
+router.post("/codereviewvideos", async (ctx: Context) => {
+    try {
+        ctx.status = 201;
+        ctx.body = {
+            games: [
+                ctx.request.body.game
+            ]
+        }
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import bodyParser from 'koa-bodyparser';
 import cors from 'koa2-cors';
 import logger from 'koa-logger';
 import healthcheckRoutes from './routes/healthcheck';
+import codereviewvideosRoutes from "./routes/codereviewvideos";
 import { config } from './config';
 const app = new Koa();
 
@@ -17,6 +18,7 @@ app.use(
 app.use(logger());
 
 app.use(healthcheckRoutes.routes());
+app.use(codereviewvideosRoutes.routes());
 
 const server = app.listen(PORT, async () => {
     console.log(`Server is running on port: ${PORT}`);
